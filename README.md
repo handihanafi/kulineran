3784d61414d74c25c0dcabd23cc96381a68c5a7f
Handi <hanafihandi96@gmail.com>
2021-10-27 00:00:42 +0700

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# kulineran
+# waptech
 
 ## Project setup
 ```
---
+++ src/components/Hero.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="hero">
+    <!-- Desktop -->
+    <div class="d-none d-md-block">
+      <div class="row mt-4">
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2>
+                Delicious Food Menu,
+                <strong>
+                  <br />
+                  in Your Gadget</strong
+                >
+              </h2>
+              <p>Ayo segera pilih dan pesan makanan favorite Anda</p>
+              <router-link class="btn btn-lg btn-success" to="/foods">
+                <b-icon-arrow-right></b-icon-arrow-right> Pesan
+              </router-link>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <img src="../assets/images/hero.png" width="100%" />
+        </div>
+      </div>
+    </div>
+
+    <!-- Mobile -->
+    <div class="d-sm-block d-md-none">
+      <div class="row mt-4">
+        <div class="col-md-6 mb-2">
+          <img src="../assets/images/hero.png" width="100%" />
+        </div>
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2>
+                Delicious Food Menu,
+                <strong>
+                  <br />
+                  in Your Gadget</strong
+                >
+              </h2>
+              <p>Ayo segera pilih dan pesan makanan favorite Anda</p>
+              <router-link class="btn btn-lg btn-success" to="/foods">
+                <b-icon-arrow-right></b-icon-arrow-right> Pesan
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
+
+<style>
+</style>
